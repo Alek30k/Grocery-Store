593b80ca6292c3dbe49f33a14c6f028556915446
Alek30k <alemank23k@gmail.com>
2024-03-21 22:19:29 -0300

    first commit

--- app/page.js
+++ app/page.js
@@ -1,10 +1,5 @@
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
-  return (
-    <div>
-      <h1>Hola asdasd asdasd asdd</h1>
-      <Button>adsas</Button>
-    </div>
-  );
+  return <div>Home</div>;
 }

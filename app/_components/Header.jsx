32e82e5e025773cfc3fcc60bf1860a0e878a508b
Alek30k <alemank23k@gmail.com>
2024-03-22 10:23:12 -0300

    last commit

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -1,5 +1,19 @@
+import { LayoutGrid } from "lucide-react";
+import Image from "next/image";
+
 const Header = () => {
-  return <div className="">Header</div>;
+  return (
+    <div className="">
+      <div className="">
+        <Image src={"/logo.png"} alt="logo" width={150} height={100} />
+        <div className="">
+          <h2 className="flex gap-2 items-center border rounded-full py-2 px-10 bg-slate-200">
+            <LayoutGrid className="w-5 h-5" /> Category
+          </h2>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Header;

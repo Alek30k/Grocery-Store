f20d72c136743b205bbca74008240b8009bd2bf3
Alek30k <alemank23k@gmail.com>
2024-03-23 10:42:00 -0300

    last commit

--- app/_utils/GlobalApi.jsx
+++ app/_utils/GlobalApi.jsx
@@ -4,4 +4,8 @@ const axiosClient = new axios.create({
   baseURL_: "http://localhost:1337/api",
 });
 
-const getCategory = () => axiosClient;
+const getCategory = () => axiosClient.get("/categories");
+
+export default {
+  getCategory,
+};

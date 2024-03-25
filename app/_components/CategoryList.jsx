121db89e79a3daf7364b844365437e7b80385fe8
Alek30k <alemank23k@gmail.com>
2024-03-25 11:17:34 -0300

    last commit

--- app/_components/CategoryList.jsx
+++ app/_components/CategoryList.jsx
@@ -7,7 +7,7 @@ const CategoryList = ({ categoryList }) => {
       <div className="grid grid-cols-3 sm:grid-cols-4 md:grid-cols-6 lg:grid-cols-7 gap-5 mt-5">
         {categoryList.map((category, index) => (
           <div
-            className="flex flex-col items-center bg-green-50 gap-2 p-4 rounded-lg group cursor-pointer hover:bg-green-100"
+            className="flex flex-col items-center bg-green-50 gap-2 p-4 rounded-lg group cursor-pointer hover:bg-green-200"
             key={index}
           >
             <Image

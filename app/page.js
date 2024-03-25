a5e2c483926b10d60d83d9ea3477fb38f158629c
Alek30k <alemank23k@gmail.com>
2024-03-25 15:29:09 -0300

    last commit

--- app/page.js
+++ app/page.js
@@ -15,25 +15,27 @@ export default async function Home() {
       <Slider sliderList={sliderList} />
       <CategoryList categoryList={categoryList} />
       <ProductList productList={productList} />
-      <div className="relative px-24 bg-green-600 w-full h-[350px] flex mt-16 justify-between items-center">
-        <div className="flex flex-col gap-8  ">
-          <h1 className="text-white font-bold text-6xl w-[650px]">
-            We Delivery your Grocery 24 Hours
-          </h1>
-          <Button
-            variant="outline"
-            className="text-primary hover:text-white hover:bg-primary border-none rounded-full  "
-          >
-            Order Now
-          </Button>
+      <div className="relative px-2  md:px-32 lg:px-64 bg-green-600 w-full h-[300px] flex mt-16 justify-between items-center ">
+        <div className="flex flex-col w-3/4">
+          <div className="">
+            <h1 className="grid grid-cols-2 lg:grid-cols-1 text-white font-bold text-2xl md:text-4xl lg:text-6xl w-[650px]">
+              We Delivery your Grocery 24 Hours
+            </h1>
+            <Button
+              variant="outline"
+              className="text-primary hover:text-white hover:bg-primary border-none rounded-full mt-6"
+            >
+              Order Now
+            </Button>
+          </div>
         </div>
-        <div className="absolute right-40 top-[-100px] }">
+        <div className=" hidden  md:flex absolute right-40 top-[-100px] w-1/4">
           <Image
             src="/moto.png"
             alt="banner"
             width={600}
             height={600}
-            className="w-full h-[450px]"
+            className="w-full h-[400px] object-cover"
           />
         </div>
       </div>

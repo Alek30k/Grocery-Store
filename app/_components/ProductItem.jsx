c5fbc685c303bd9ed42cf00a843c09c5846c089d
Alek30k <alemank23k@gmail.com>
2024-03-25 11:32:38 -0300

    last commit

---
+++ app/_components/ProductItem.jsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+
+const ProductItem = ({ product }) => {
+  return (
+    <div className="">
+      <Image
+        src={
+          process.env.NEXT_PUBLIC_BACKEND_BASE_URL +
+          product?.attributes?.images?.data[0]?.attributes?.url
+        }
+        width={500}
+        height={200}
+        alt="image"
+      />
+    </div>
+  );
+};
+
+export default ProductItem;
--- app/_components/ProductList.jsx
+++ app/_components/ProductList.jsx
@@ -1,12 +1,14 @@
+import ProductItem from "./ProductItem";
+
 const ProductList = ({ productList }) => {
   return (
     <div className="mt-10">
       <h2 className="text-green-600 font-bold text-2xl">
         Our Popular Products
       </h2>
-      <div className=''>
-        {productList.map((product, index) =>(
-
+      <div className="">
+        {productList.map((product, index) => (
+          <ProductItem product={product} key={index} />
         ))}
       </div>
     </div>

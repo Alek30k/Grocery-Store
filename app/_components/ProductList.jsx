b71ecab1e555e5134b60cc7016dfedf85df4cec0
Alek30k <alemank23k@gmail.com>
2024-03-25 11:26:40 -0300

    last commit

--- app/_components/ProductList.jsx
+++ app/_components/ProductList.jsx
@@ -1,5 +1,16 @@
-const ProductList = () => {
-  return <div className="">ProductList</div>;
+const ProductList = ({ productList }) => {
+  return (
+    <div className="mt-10">
+      <h2 className="text-green-600 font-bold text-2xl">
+        Our Popular Products
+      </h2>
+      <div className=''>
+        {productList.map((product, index) =>(
+
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default ProductList;

256219e4d96431d5689b9ce2d45c1442ce9d9dba
Alek30k <alemank23k@gmail.com>
2024-03-25 12:52:22 -0300

    last commit

--- app/_components/ProductItem.jsx
+++ app/_components/ProductItem.jsx
@@ -1,8 +1,9 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 const ProductItem = ({ product }) => {
   return (
-    <div className="">
+    <div className="p-2 md:p-6 flex flex-col items-center justify-center gap-3 border rounded-lg ">
       <Image
         src={
           process.env.NEXT_PUBLIC_BACKEND_BASE_URL +
@@ -11,7 +12,19 @@ const ProductItem = ({ product }) => {
         width={500}
         height={200}
         alt={product?.attributes?.name}
+        className="h-[200px] w-[200px] object-contain"
       />
+      <h2 className="font-bold text-lg">{product?.attributes?.name}</h2>
+      {product?.attributes?.sellingPrice && (
+        <h2 className="">${product?.attributes?.sellingPrice}</h2>
+      )}
+      <h2 className="font-bold line-through">${product?.attributes?.mrp}</h2>
+      <Button
+        variant="outline"
+        className="text-primary hover:text-white hover:bg-primary"
+      >
+        Add to cart
+      </Button>
     </div>
   );
 };

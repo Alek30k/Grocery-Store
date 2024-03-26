545eb4c218afc0dbf945b2e6f59bb7da93f52677
Alek30k <alemank23k@gmail.com>
2024-03-25 22:49:25 -0300

    last commit

--- app/_components/ProductItemDetail.jsx
+++ app/_components/ProductItemDetail.jsx
@@ -1,5 +1,26 @@
+import Image from "next/image";
+
 const ProductItemDetail = ({ product }) => {
-  return <div className="">ProductItemDetail</div>;
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 p-7 bg-white text-black">
+      <Image
+        src={
+          process.env.NEXT_PUBLIC_BACKEND_BASE_URL +
+          product?.attributes?.images?.data[0]?.attributes?.url
+        }
+        alt="image"
+        width={300}
+        height={300}
+        className="bg-slate-200 p-5 h-[320px] w-[300px] object-contain pointer-events-none rounded-lg"
+      />
+      <div>
+        <h2 className="text-2xl font-bold">{product?.attributes?.name}</h2>
+        <h2 className="text-sm font-bold text-gray-500">
+          {product?.attributes?.description}
+        </h2>
+      </div>
+    </div>
+  );
 };
 
 export default ProductItemDetail;

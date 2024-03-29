7b763061b19cf794ce3b7061eba040574bee5e47
Alek30k <alemank23k@gmail.com>
2024-03-29 10:26:29 -0300

    last commit

--- app/(auth)/create-account/page.jsx
+++ app/(auth)/create-account/page.jsx
@@ -38,7 +38,7 @@ const CreateAccount = () => {
   };
 
   return (
-    <div className="flex items-baseline justify-center my-10">
+    <div className="flex items-baseline justify-center my-20">
       <div className="flex flex-col items-center justify-center p-10 bg-slate-100 border border-gray-200">
         <Image src="/logo.png" alt="logo" width={200} height={200} />
         <h2 className="font-bold text-2xl">Create an Account</h2>

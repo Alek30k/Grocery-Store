67f25c468dd5c56a1e96ba4b80a433b1544e94da
Alek30k <alemank23k@gmail.com>
2024-03-29 23:27:18 -0300

    last commit

--- app/_components/CartItemList.jsx
+++ app/_components/CartItemList.jsx
@@ -1,3 +1,4 @@
+import { TrashIcon } from "lucide-react";
 import Image from "next/image";
 
 const CartItemList = ({ cartItemList }) => {
@@ -5,13 +6,25 @@ const CartItemList = ({ cartItemList }) => {
     <div className="">
       <div className="">
         {cartItemList.map((cart, index) => (
-          <div className="" key={index}>
-            <Image
-              src={process.env.NEXT_PUBLIC_BACKEND_BASE_URL + cart.image}
-              width={70}
-              height={70}
-              alt={cart.name}
-            />
+          <div
+            className="flex justify-between items-center p-2 mb-5"
+            key={index}
+          >
+            <div className="flex gap-6 items-center">
+              <Image
+                src={process.env.NEXT_PUBLIC_BACKEND_BASE_URL + cart.image}
+                width={70}
+                height={70}
+                alt={cart.name}
+                className="border p-2"
+              />
+              <div className="">
+                <h2 className="font-bold">{cart.name}</h2>
+                <h2 className="">Quantity {cart.quantity}</h2>
+                <h2 className="text-lg  font-bold">$ {cart.amount}</h2>
+              </div>
+            </div>
+            <TrashIcon />
           </div>
         ))}
       </div>

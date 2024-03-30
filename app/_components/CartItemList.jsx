eb324af68f3a5467f735edd0c6788c34de9b17f5
Alek30k <alemank23k@gmail.com>
2024-03-29 23:09:51 -0300

    last commit

--- app/_components/CartItemList.jsx
+++ app/_components/CartItemList.jsx
@@ -1,5 +1,22 @@
+import Image from "next/image";
+
 const CartItemList = ({ cartItemList }) => {
-  return <div className="">CartItemList</div>;
+  return (
+    <div className="">
+      <div className="">
+        {cartItemList.map((cart, index) => (
+          <div className="" key={index}>
+            <Image
+              src={process.env.NEXT_PUBLIC_BACKEND_BASE_URL + cart.image}
+              width={70}
+              height={70}
+              alt={cart.name}
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default CartItemList;

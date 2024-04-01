b9eb86c3bac336d8ca4b33bd2919a3dc74c7c4c8
Alek30k <alemank23k@gmail.com>
2024-04-01 16:33:50 -0300

    last commit

--- app/_components/ProductList.jsx
+++ app/_components/ProductList.jsx
@@ -7,9 +7,18 @@ const ProductList = ({ productList }) => {
         Our Popular Products
       </h2>
       <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 mt-6">
-        {productList.map(
-          (product, index) =>
-            index < 8 && <ProductItem product={product} key={index} />
+        {productList.length === 0 ? (
+          <div className="flex flex-col gap-10 items-center justify-center w-[100vw]">
+            <h1 className="text-primary font-bold text-5xl">Oops!</h1>
+            <p className="text-gray-400 text-2xl ">
+              It seems that this category does not have products!
+            </p>
+          </div>
+        ) : (
+          productList.map(
+            (product, index) =>
+              index < 8 && <ProductItem product={product} key={index} />
+          )
         )}
       </div>
     </div>
